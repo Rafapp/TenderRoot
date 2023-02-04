94a87b1d792e655a0e3c20f09cce180f51047200
ASokry <anthonysokry11@gmail.com>
2023-02-04 13:44:35 -0800

    update boiler plate

--- Source/main.lua
+++ Source/main.lua
@@ -3,6 +3,8 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+local gfx <const> = playdate.graphics
+
 local function resetTimer()
 	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
 end
@@ -16,6 +18,18 @@ initialize()
 
 --runs 30fps, can be somehow changed
 function playdate.update()
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		
+	end
+	if playdate.buttonIsPressed(playdate.kButtonRight) then
+		
+	end
+	if playdate.buttonIsPressed(playdate.kButtonDown) then
+		
+	end
+	if playdate.buttonIsPressed(playdate.kButtonLeft) then
+		
+	end
 
 	playdate.timer.updateTimers() --always update all timers at the end of update loop, even if you dont use timer related variables
 	gfx.sprite.update() --tells system to update every sprite on the draw list
